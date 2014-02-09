0a9cfaeba135eecac5d4ca9c6d8d6fc8df306d00
LestaD <lestad@bk.ru>
2014-02-10 02:29:44 +0400

    v2.0.8

--- engine.php
+++ engine.php
@@ -60,46 +60,6 @@ include DIR_CORE . 'db.class.php';
 $phenol->db = new Core\Database( $phenol );
 
 
-function ParseTomlFile($file) {
-	return (object)\Toml\Parser2::fromFile( $file );
-}
-
-function safefilerewrite($fileName, $dataToSave)
-{    if ($fp = fopen($fileName, 'w'))
-    {
-        $startTime = microtime();
-        do
-        {            $canWrite = flock($fp, LOCK_EX);
-           // If lock not obtained sleep for 0 - 100 milliseconds, to avoid collision and CPU load
-           if(!$canWrite) usleep(round(rand(0, 100)*1000));
-        } while ((!$canWrite)and((microtime()-$startTime) < 1000));
- 
-        //file was locked so now we can store information
-        if ($canWrite)
-        {            fwrite($fp, $dataToSave);
-            flock($fp, LOCK_UN);
-        }
-        fclose($fp);
-    }
- 
-}
-
-function write_ini_file($array, $file)
-{
-    $res = array();
-    foreach($array as $key => $val)
-    {
-        if(is_array($val))
-        {
-            $res[] = "[$key]";
-            foreach($val as $skey => $sval) $res[] = "$skey = ".(is_numeric($sval) ? $sval : '"'.$sval.'"');
-        }
-        else $res[] = "$key = ".(is_numeric($val) ? $val : '"'.$val.'"');
-    }
-    safefilerewrite($file, implode("\r\n", $res));
-}
-
-
 class Ph {
 	private static $imported = array();
 	
@@ -147,9 +107,4 @@ class Ph {
 		}
 	}
 }
-
-// is_subclass_of()
-
-
-
 
